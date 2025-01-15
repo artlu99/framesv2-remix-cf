a0b8905d2015621c4e7fa56ea0730bb1df76265a
artlu99 <alu99.sm@gmail.com>
2025-01-15 08:38:41 -0500

    better semantic html

--- app/components/WalletsList.client.tsx
+++ app/components/WalletsList.client.tsx
@@ -13,9 +13,10 @@ const WalletsList = (props: WalletsListProps) => {
   return (
     <article className="prose">
       {wallets.map((wallet) => (
-        <div
+        <Button
           key={wallet.label}
-          className="flex gap-2 my-4 cursor-pointer"
+          variant="outline"
+          className="flex w-full p-8 dark:text-gray-300 my-4"
           onClick={() => {
             navigator.clipboard.writeText(wallet.address);
             toast({
@@ -27,28 +28,22 @@ const WalletsList = (props: WalletsListProps) => {
             });
           }}
         >
-          <Button
-            variant="outline"
-            className="flex w-full p-8 dark:text-gray-300"
-          >
-            <div className="flex-1 text-left text-sm">
-              <div className="">
-                <span className="font-bold">{wallet.tooltip}</span>{" "}
-                {wallet.label}
-              </div>
-              <div className="">
-                <span className="whitespace-nowrap flex items-center">
-                  {wallet.address.length > 11
-                    ? `${wallet.address.slice(0, 6)}...${wallet.address.slice(
-                        -4
-                      )}`
-                    : wallet.address}
-                  <RiFileCopyLine size="1em" className="ml-1" />
-                </span>
-              </div>
+          <div className="flex-1 text-left text-sm">
+            <div>
+              <span className="font-bold">{wallet.tooltip}</span> {wallet.label}
             </div>
-          </Button>
-        </div>
+            <div>
+              <span className="whitespace-nowrap flex items-center">
+                {wallet.address.length > 11
+                  ? `${wallet.address.slice(0, 6)}...${wallet.address.slice(
+                      -4
+                    )}`
+                  : wallet.address}
+                <RiFileCopyLine size="1em" className="ml-1" />
+              </span>
+            </div>
+          </div>
+        </Button>
       ))}
     </article>
   );
