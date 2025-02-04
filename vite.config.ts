f3945cb074d2bd397c09fe06c17578fe5b407ce0
artlu99 <alu99.sm@gmail.com>
2025-02-04 14:50:16 -0500

    cleaner, modern configs

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,17 @@ export default defineConfig({
   },
   plugins: [
     cloudflareDevProxyVitePlugin(),
-    remix(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+      // Add this to handle .client suffix
+      ignoredRouteFiles: ["**/.*", "**/*.client.*"],
+    }),
     devServer({
       adapter,
       entry: "server.ts",
